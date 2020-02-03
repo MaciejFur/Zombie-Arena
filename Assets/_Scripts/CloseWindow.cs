9c1fc75f4050b4d9864888cbae0f66b3970d10fb
Maciej Furdyna <MaciejFur@users.noreply.github.com>
2020-02-03 15:38:32 +0100

    minor update

---
+++ Assets/_Scripts/CloseWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseWindow : MonoBehaviour
+{
+    bool closing = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                closing = !closing;
+            }
+        }
+    }
+}
